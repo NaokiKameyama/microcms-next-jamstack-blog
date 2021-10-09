29cf9d3f0f004b7eab45155acae7ade836fa84e4
Naoki-Kameyama <atukan0930@gmail.com>
2021-10-10 02:57:04 +0900

    aaa

--- components/blog/index.js
+++ components/blog/index.js
@@ -1,4 +1,5 @@
 import styles from '../../styles/Home.module.scss';
+import Image from 'next/image'
 
 export const Blog = ({ blog }) => {
   return (
@@ -6,6 +7,7 @@ export const Blog = ({ blog }) => {
       <h1 className={styles.title}>{blog.title}</h1>
       <p className={styles.publishedAt}>{blog.publishedAt}</p>
       <p className="category">{blog.category && `${blog.category.name}`}</p>
+      <Image src={blog.image.url} width={blog.image.width} height={blog.image.height} alt="My avatar" />
       <div
         dangerouslySetInnerHTML={{
           __html: `${blog.body}`,
