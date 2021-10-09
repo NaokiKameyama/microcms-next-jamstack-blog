0cbffcee6ab0968e2a0fc168499e3e25cf037d61
Naoki-Kameyama <atukan0930@gmail.com>
2021-10-09 16:03:34 +0900

    feat: blogコンポーネントの共通化

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,19 +1,10 @@
+import { Blog } from "../../components/blog";
 import { client } from "../../libs/client";
 import styles from '../../styles/Home.module.scss';
 
 export default function BlogId({ blog }) {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>{blog.title}</h1>
-      <p className={styles.publishedAt}>{blog.publishedAt}</p>
-      <p className="category">{blog.category && `${blog.category.name}`}</p>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `${blog.body}`,
-        }}
-        className={styles.post}
-      />
-    </main>
+    <Blog blog={blog} />
   );
 }
 
--- pages/preview/[id].js
+++ pages/preview/[id].js
@@ -1,20 +1,13 @@
+import { Blog } from "../../components/blog";
 import { client } from "../../libs/client";
 import styles from '../../styles/Home.module.scss';
 
 export default function BlogId({ blog }) {
   return (
-    <main className={styles.main}>
-      ※Preview mode
-      <h1 className={styles.title}>{blog.title}</h1>
-      <p className={styles.publishedAt}>{blog.publishedAt}</p>
-      <p className="category">{blog.category && `${blog.category.name}`}</p>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `${blog.body}`,
-        }}
-        className={styles.post}
-      />
-    </main>
+    <>
+      <p>※Preview Mode</p>
+      <Blog blog={blog} />
+    </>
   );
 }
 
