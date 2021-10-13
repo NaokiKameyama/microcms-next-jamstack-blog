f7717f0704fe4695f5ac2934bb17c7e82cf53ac2
NaokiKameyama <atukan0930@gmail.com>
2021-10-13 15:38:55 +0900

    noreferrer対応

--- components/home/index.js
+++ components/home/index.js
@@ -55,7 +55,7 @@ export default function Home({ blog }) {
               工学修士→某通信企業→Fintech企業。<br/>
               職種は、ProductOps Engineer。<br/>
               趣味はFlutterのアプリ開発やお金の勉強にハマっていまして、最近
-              <a target="_blank" href="https://apps.apple.com/jp/app/%E8%8B%B1%E5%8D%98%E8%AA%9E%E9%80%9A%E7%9F%A5/id1582213197">「英単語通知」</a>というアプリをリリースしました。<br />
+              <a target="_blank" rel="noreferrer" href="https://apps.apple.com/jp/app/%E8%8B%B1%E5%8D%98%E8%AA%9E%E9%80%9A%E7%9F%A5/id1582213197">「英単語通知」</a>というアプリをリリースしました。<br />
               働き方、技術、金融のジャンルに興味があるので、それらに関することを書いていこうと思います。
               </div>
               <div className={s['icon-container']}>
