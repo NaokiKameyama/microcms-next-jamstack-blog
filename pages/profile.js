ed2ea53bbc9412c06ea5e4abe55e601755721658
Naoki-Kameyama <atukan0930@gmail.com>
2021-10-16 17:03:00 +0900

    deploy

--- pages/profile.js
+++ pages/profile.js
@@ -1,9 +1,10 @@
 // import Profile from "../components/profile";
 import Contact from "../components/contact"
+import Profile from "../components/profile"
 
 export default function profile() {
   return (
-    // <Profile />
-    <Contact />
+    <Profile />
+    // <Contact />
   );
 }
