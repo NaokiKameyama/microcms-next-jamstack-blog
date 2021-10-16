c27ca70fc3f433d75b754be8f5e77a11a41c843e
Naoki-Kameyama <atukan0930@gmail.com>
2021-10-16 17:25:16 +0900

    aaaa

--- pages/profile.js
+++ pages/profile.js
@@ -1,6 +1,6 @@
 // import Profile from "../components/profile";
 // import Contact from "../components/contact"
-import Profile from "../components/profile"
+import Profile from "../components/profile2";
 
 export default function profile() {
   return (
