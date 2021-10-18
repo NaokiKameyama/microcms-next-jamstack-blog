b3e910f3295ce81cd2531ac2aa024dacc8457dd7
Naoki-Kameyama <atukan0930@gmail.com>
2021-10-18 20:27:19 +0900

    google analytics対応

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 import '../styles/globals.css'
 import Layout from '../components/layout'
+import GoogleAnalytics from '../components/GoogleAnalytics' //https://panda-program.com/posts/nextjs-google-analytics
+import usePageView from '../hooks/usePageView'
+
+import { useRouter } from "next/router";
+import React, { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  usePageView()
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <GoogleAnalytics />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
