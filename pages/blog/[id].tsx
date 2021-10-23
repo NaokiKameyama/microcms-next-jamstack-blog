7d0afe722cd2558e060cc6964840712062e5d962
NaokiKameyama <atukan0930@gmail.com>
2021-10-23 16:00:57 +0900

    prettier husky

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,13 +1,13 @@
 // import { Blog } from "../../components/blog";
 import { client } from "../../libs/client";
-import styles from '../../styles/Home.module.scss';
-import cheerio from 'cheerio';
-import hljs from 'highlight.js'
-import 'highlight.js/styles/vs2015.css';
-import Article from "../../components/Article"
-import CommonMeta from "../../components/commonMeta"
+import styles from "../../styles/Home.module.scss";
+import cheerio from "cheerio";
+import hljs from "highlight.js";
+import "highlight.js/styles/vs2015.css";
+import Article from "../../components/Article";
+import CommonMeta from "../../components/commonMeta";
 
-export default function BlogId({blog ,highlightedBody}) {
+export default function BlogId({ blog, highlightedBody }) {
   return (
     <>
       <CommonMeta blog={blog} />
@@ -18,29 +18,32 @@ export default function BlogId({blog ,highlightedBody}) {
 
 // 静的生成のためのパスを指定します
 export const getStaticPaths = async () => {
-  const data: {contents:[]} = await client.get({ endpoint: "blog" });
-  const paths = data.contents.map((content: {id: string}) => `/blog/${content.id}`);
+  const data: { contents: [] } = await client.get({ endpoint: "blog" });
+  const paths = data.contents.map(
+    (content: { id: string }) => `/blog/${content.id}`
+  );
   return { paths, fallback: false };
 };
 
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async (context) => {
   const id = context.params.id;
-  const data: {body: string} = await client.get({ endpoint: "blog", contentId: id });
-  
-  const $ = cheerio.load(data.body);    // data.bodyはmicroCMSから返されるリッチエディタ部分
-  $('pre code').each((_, elm) => {
+  const data: { body: string } = await client.get({
+    endpoint: "blog",
+    contentId: id,
+  });
+
+  const $ = cheerio.load(data.body); // data.bodyはmicroCMSから返されるリッチエディタ部分
+  $("pre code").each((_, elm) => {
     const result = hljs.highlightAuto($(elm).text());
     $(elm).html(result.value);
-    $(elm).addClass('hljs');
+    $(elm).addClass("hljs");
   });
 
   return {
     props: {
       blog: data,
-      highlightedBody:$.html()
+      highlightedBody: $.html(),
     },
   };
 };
-
-
