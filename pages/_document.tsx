314c34ebd45939754547491bec5e9bbbd71f8920
NaokiKameyama <atukan0930@gmail.com>
2021-10-24 00:26:46 +0900

    adsenseスクリプトの埋め込み

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,11 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script
+            data-ad-client="ca-pub-6575946142497456"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
           {/* Google Tag Manager*/}
           <script
             dangerouslySetInnerHTML={{
