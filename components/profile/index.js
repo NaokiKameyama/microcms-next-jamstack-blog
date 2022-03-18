5a5ba9c745f3bbc6e3b5c69f54c05f696380afab
Naoki-Kameyama <atukan0930@gmail.com>
2022-03-18 09:25:07 +0900

    typo

--- components/profile/index.js
+++ components/profile/index.js
@@ -24,7 +24,7 @@ export default function Profile({}) {
               <br />
               主に「技術、金融、働き方」に関する情報発信をしています。
               <br />
-              (本サイトは、Next.js, TypesScript, microCMS, Vercel等の技術を使って、0から構築しています。)
+              (本サイトは、Next.js, TypeScript, microCMS, Vercel等の技術を使って、0から構築しています。)
               <br />
             </div>
             <div>
